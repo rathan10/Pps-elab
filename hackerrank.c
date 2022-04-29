2f0272d77c6bd1ccbb941fb320f0e70a339d76e0
rathan10 <104297716+rathan10@users.noreply.github.com>
2022-04-29 23:19:10 +0530

    Create hackerrank.c

---
+++ hackerrank.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+float tractLand,tractLandAcred;
+
+scanf("%f",&tractLand);
+
+tractLandAcred=(float)tractLand/43560;
+
+printf("%.2f sq.ft is equal to %.2f acres",tractLand,tractLandAcred);
+
+               return 0;
+
+}
